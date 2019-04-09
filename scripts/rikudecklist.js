1b3dad3011058c54bd1fc3f18d3a86bb5c8dc01f
Performance Media <szeszej@gmail.com>
2019-04-09 15:07:53 +0200

    adding second decklist

---
+++ scripts/rikudecklist.js
@@ -0,0 +1,89 @@
+//"baza danych" z decklistą Xantchy
+var deckList = `1x Acidic Slime
+1x Body Double
+1x Coiling Oracle
+1x Crater Hellion
+1x Deadeye Navigator
+1x Dire Fleet Daredevil
+1x Duplicant
+1x Eternal Witness
+1x Farhaven Elf
+1x Fauna Shaman
+1x Flametongue Kavu
+1x Frilled Mystic
+1x Genesis
+1x Greenwarden of Murasa
+1x Gyre Engineer
+1x Haze Frog
+1x Inferno Titan
+1x Meteor Golem
+1x Mindclaw Shaman
+1x Mulldrifter
+1x Mystic Snake
+1x Nephalia Smuggler
+1x Nimble Obstructionist
+1x Oracle of Mul Daya
+1x Pelakka Wurm
+1x Peregrine Drake
+1x Rattleclaw Mystic
+1x Ravager Wurm
+1x Reclamation Sage
+1x Riftsweeper
+1x Scavenging Ooze
+1x Seedborn Muse
+1x Somberwald Sage
+1x Sower of Temptation
+1x Squee, Goblin Nabob
+1x Stunt Double
+1x Teferi, Mage of Zhalfir
+1x Temur Sabertooth
+1x Thragtusk
+1x Venser, Shaper Savant
+1x Wood Elves
+1x Alchemist's Refuge
+1x Breeding Pool
+1x Cinder Glade
+1x Command Tower
+1x Desolate Lighthouse
+1x Exotic Orchard
+1x Flooded Grove
+6x Forest
+1x Frontier Bivouac
+4x Island
+1x Memorial to Genius
+1x Memorial to Unity
+5x Mountain
+1x Reliquary Tower
+1x Rootbound Crag
+1x Scavenger Grounds
+1x Shivan Reef
+1x Steam Vents
+1x Temple of Abandon
+1x Temple of Epiphany
+1x Temple of Mystery
+1x Tolaria West
+1x Windswept Heath
+1x Yavimaya Coast
+1x Birthing Pod
+1x Crystal Shard
+1x Sol Ring
+1x Chord of Calling
+1x Cyclonic Rift
+1x Ghostly Flicker
+1x Krosan Grip
+1x Plasm Capture
+1x Summoning Trap
+1x Worldly Tutor
+1x Cultivate
+1x Harmonize
+1x Kodama's Reach
+1x Primal Command
+1x Sylvan Scrying
+1x Urban Evolution
+1x Cryptolith Rite
+1x Equilibrium
+1x Flameshadow Conjuring
+1x Guardian Project
+1x Sneak Attack
+1x Survival of the Fittest
+1x Riku of Two Reflections *CMDR*`;
