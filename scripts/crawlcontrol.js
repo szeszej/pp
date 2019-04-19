3f2044e27059a404bd2eb954b5647060450d5ff4
szeszej <szeszej@gmail.com>
2019-04-19 22:51:47 +0200

    added prototype slideshow (copy-paste from W3S)

--- scripts/crawlcontrol.js
+++ scripts/crawlcontrol.js
@@ -26,3 +26,32 @@ skipButton.addEventListener("click", function() { //funkcja, która kończy teks
   skipButton.style.display = "none";
   afterCrawl.style.animation = "show 2s ease-in 0s 1 normal forwards";
 });
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "flex";
+  dots[slideIndex-1].className += " active";
+}
