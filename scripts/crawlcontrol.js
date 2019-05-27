b92ce9aca5e60abdc8dbda5ee7c96c5076c1dcb3
szeszej <szeszej@gmail.com>
2019-05-27 22:53:36 +0200

    started working on refactoring crawlcontrol

--- scripts/crawlcontrol.js
+++ scripts/crawlcontrol.js
@@ -1,31 +1,34 @@
-var swCrawlEnd = document.querySelector("#lastparagraph"); //znajduje pusty paragraf na końcu przewijanego tekstu na stronie głównej
-var afterCrawl = document.querySelector(".aftercrawl"); //znajduje div, który ma być wyświetlony po przewinięcu się tekstu
-var swCrawl = document.querySelector(".crawl"); //znajduje tekst przewijany
-var skipButton = document.querySelector(".skip"); //znajduje guzik pominięcia
+var animationWatch = function() {
+  let swCrawl = document.querySelector(".crawl"); //znajduje tekst przewijany
+  let swCrawlEnd = document.querySelector("#lastparagraph"); //znajduje pusty paragraf na końcu przewijanego tekstu na stronie głównej
+  let afterCrawl = document.querySelector(".aftercrawl"); //znajduje div, który ma być wyświetlony po przewinięcu się tekstu
+  let skipButton = document.querySelector(".skip"); //znajduje guzik pominięcia
 
-var header = document.querySelector("header"); //znajduje header
-var nav = document.querySelector("nav"); //znajduje nawigację
-var fade = document.querySelector(".fade"); //znajduje div, który "znika" animację
+  let header = document.querySelector("header"); //znajduje header
+  let nav = document.querySelector("nav"); //znajduje nawigację
+  let fade = document.querySelector(".fade"); //znajduje div, który "znika" animację
+  let menuHeight = parseInt(window.getComputedStyle(header).height) + parseInt(window.getComputedStyle(nav).height) + parseInt(window.getComputedStyle(fade).height); //oblicza łączną wysokość elementów, żeby wyłączyć animację, kiedy jest poza obszarem widocznym
 
-var menuHeight = parseInt(window.getComputedStyle(header).height) + parseInt(window.getComputedStyle(nav).height) + parseInt(window.getComputedStyle(fade).height); //oblicza łączną wysokość elementów, żeby wyłączyć animację, kiedy jest poza obszarem widocznym
-
-var animationWatch = setInterval(function() { //funkcja która sprawdza co pewien czas, gdzie znajduje się (oś y) osatni paragraf w tekście przewijanym
-  let crawlPosition = swCrawlEnd.getBoundingClientRect();
-  if (crawlPosition.y < menuHeight - 50 && crawlPosition.y > 0) { //jeśli jest odpowiednio wysoko (poza obszarem widocznym), to włączamy animację po przewinięciu się tekstu i przestajemy liczyć
-    afterCrawl.style.animation = "show 2s ease-in 0s 1 normal forwards";
+  skipButton.addEventListener("click", function() { //funkcja, która kończy tekst przewijany i wyświetla animację po przewinięciu, kiedy klikniemy na przycisk
+    swCrawl.style.display = "none";
+    clearInterval(animationWatch);
     fade.style.display = "none";
     skipButton.style.display = "none";
-    clearInterval(animationWatch);
-  }
-}, 500);
+    afterCrawl.style.animation = "show 2s ease-in 0s 1 normal forwards";
+  });
 
-skipButton.addEventListener("click", function() { //funkcja, która kończy tekst przewijany i wyświetla animację po przewinięciu, kiedy klikniemy na przycisk
-  swCrawl.style.display = "none";
-  clearInterval(animationWatch);
-  fade.style.display = "none";
-  skipButton.style.display = "none";
-  afterCrawl.style.animation = "show 2s ease-in 0s 1 normal forwards";
-});
+  setInterval(function() { //funkcja która sprawdza co pewien czas, gdzie znajduje się (oś y) osatni paragraf w tekście przewijanym
+    let crawlPosition = swCrawlEnd.getBoundingClientRect();
+    if (crawlPosition.y < menuHeight - 50 && crawlPosition.y > 0) { //jeśli jest odpowiednio wysoko (poza obszarem widocznym), to włączamy animację po przewinięciu się tekstu i przestajemy liczyć
+      afterCrawl.style.animation = "show 2s ease-in 0s 1 normal forwards";
+      fade.style.display = "none";
+      skipButton.style.display = "none";
+      clearInterval(animationWatch);
+    }
+  }, 500);
+
+};
+animationWatch();
 
 var slideIndex = 1; // zaczynamy od slajdu nr 1
 var automaticSlideChange = setInterval(function() { //zmieniamy slajd automatycznie co 8 sekund
